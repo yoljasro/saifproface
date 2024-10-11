4900b95ca8f7f0b04daf475352fad8110f1d8327
yoljasro <saidaliyevjasur450@gmail.com>
2024-10-11 17:19:54 +0500

    changing

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,15 +6,27 @@ interface Teacher {
   src: string;
   title: string;
   description: string;
-  pdf?: string; // Optional
+  pdf?: string;
   bgImage: string;
   biografy?: string;
-  teach? : string;
+  teach?: string;
   skill?: string;
   titleone?: string;
   titletwo?: string;
+  teachtwo? : string;
   titlethree?: string;
   lastyebat? : string;
+  teachthree? : string;
+  teachfourth? : string;
+  teachfive? : string;
+  teachsix? : string;
+  skilltwo?: string;
+  skillthree?: string;
+  skillfourth?: string;
+  skillfive?: string;
+  skillsix? : string;
+  skillseven? : string;
+  skilleight? : string;
 }
 
 interface ModalProps {
@@ -35,8 +47,20 @@ const Modal: React.FC<ModalProps> = ({ onClose, teacher }) => {
         <p className={styles.bio}>{teacher.biografy}</p>
         <p className={styles.biografytwo}>{teacher.titletwo}</p>
         <p className={styles.biotwo}>{teacher.teach}</p>
+        <p className={styles.biotwo}>{teacher.teachtwo}</p>
+        <p className={styles.biotwo}>{teacher.teachthree}</p>
+        <p className={styles.biotwo}>{teacher.teachfourth}</p>
+        <p className={styles.biotwo}>{teacher.teachfive}</p>
+        <p className={styles.biotwo}>{teacher.teachsix}</p>
         <p className={styles.biografythree}>{teacher.titlethree}</p>
         <p className={styles.biothree}>{teacher.skill}</p>
+        <p className={styles.biothree}>{teacher.skilltwo}</p>
+        <p className={styles.biothree}>{teacher.skillthree}</p>
+        <p className={styles.biothree}>{teacher.skillfourth}</p>
+        <p className={styles.biothree}>{teacher.skillfive}</p>
+        <p className={styles.biothree}>{teacher.skillsix}</p>
+        <p className={styles.biothree}>{teacher.skillseven}</p>
+        <p className={styles.biothree}>{teacher.skilleight}</p>
         </div>
       
       </div>
