c8945dd71f7f102e0d8c2ae1454bbb5fd5b4c8c1
yoljasro <saidaliyevjasur450@gmail.com>
2025-10-24 03:31:58 +0500

    changing design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import styles from './index.module.sass';
+import React, { useState, useEffect } from "react";
+import styles from "./index.module.sass";
 import { useTranslations } from "next-intl";
 
 export const Header = () => {
@@ -7,28 +7,21 @@ export const Header = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [fade, setFade] = useState(true);
 
-  const images = [
-    '/assets/img/image1.jpg',
-    '/assets/img/fasad.jpg'
-  ];
+  const images = ["/assets/img/image1.jpg", "/assets/img/fasad.jpg"];
 
   useEffect(() => {
-    // Slayder uchun interval
     const interval = setInterval(() => {
       setFade(false);
       setTimeout(() => {
-        setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        setCurrentImage((p) => (p + 1) % images.length);
         setFade(true);
-      }, 500); // fade-out effect for 500ms
-    }, 4000); // image changes every 4 seconds
-
-    return () => {
-      clearInterval(interval);
-    };
+      }, 500);
+    }, 4000);
+    return () => clearInterval(interval);
   }, [images.length]);
 
   return (
-    <div className={styles.header} id='aboutus'>
+    <div className={styles.header} id="aboutus">
       <div className={styles.header__container}>
         {/* Video Background */}
         <video
@@ -38,8 +31,7 @@ export const Header = () => {
           muted
           playsInline
         >
-          <source src="/assets/img/saifvideo.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
+          <source src="/assets/img/videomain.mov" type="video/mp4" />
         </video>
         <video
           className={`${styles.header__video} ${styles.mobileVideo}`}
@@ -49,12 +41,18 @@ export const Header = () => {
           playsInline
         >
           <source src="/assets/img/phonevideo.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
+
+        {/* COLOR OVERLAY (#856CA9, semi-transparent) */}
+        <div className={styles.header__overlay} />
+
+        {/* Text */}
         <div className={styles.header__text}>
-          <p className={styles.header__name}>{t("welcome")}</p>
-          <p className={styles.header__descone}>{t("aboutBlock.title")}</p>
-          <p className={styles.header__desc}>{t("aboutBlock.desc")}</p>
+          <div className={styles.header__textBox}>
+            <p className={styles.header__name}>{t("welcome")}</p>
+            <p className={styles.header__descone}>{t("aboutBlock.title")}</p>
+            <p className={styles.header__desc}>{t("aboutBlock.desc")}</p>
+          </div>
         </div>
       </div>
     </div>
